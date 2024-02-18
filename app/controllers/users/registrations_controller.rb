d733373fdda4efe0f69089029f0ed79899fbdde4
alexawan63 <118731133+alexawan63@users.noreply.github.com>
2024-02-19 01:16:36 +0500

    Integrated active admin (#2)
    * Configured active admin
    
    * Fixed user login/signup issue after active admin
    
    * Added users section in active admin

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  skip_before_action :verify_authenticity_token
+
   respond_to :json
 
   private
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :verify_authenticity_token
+
   respond_to :json
 
   private
